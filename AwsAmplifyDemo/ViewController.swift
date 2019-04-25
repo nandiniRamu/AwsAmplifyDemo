241dea19fcccf9581ac029a4937cc8242cd7ed8b
Nandini Ramu <nandini@Nandinis-MBP.attlocal.net>
2019-04-25 12:16:41 -0700

    empty project

---
+++ AwsAmplifyDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AwsAmplifyDemo
+//
+//  Created by Nandini Ramu on 25/04/19.
+//  Copyright © 2019 Nandini Ramu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
